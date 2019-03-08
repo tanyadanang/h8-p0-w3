6c93a4b9b457ec627b2841ce81664a3c5f255828
tanyadanang <47728820+tanyadanang@users.noreply.github.com>
2019-03-09 01:44:39 +0700

    Update exercises2-mengakses-nilai.js

--- exercises2-mengakses-nilai.js
+++ exercises2-mengakses-nilai.js
@@ -1,11 +1,12 @@
-//Exercises 2 
+//[Exercises 2] Tantangan Array 1 (Mengakses Nilai dalam Array)
 
 function balikString(str){
-    var output =''
-    for( i=str.length-1;i>=0;i--){
-        output+=str[i]
-    }return output
+    var output = ''
+    for (var i = str.length-1;i >= 0; i--){
+        output +=str[i]
+    }
+    return output
 }
 
 console.log(balikString('Hello world!'))
-console.log(balikString('Danang Wahyudi'))
+console.log (balikString('Danang Wahyudi'))
